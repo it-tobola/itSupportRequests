34f38c1e293dc19e0e59915dfed761f2877bd906
wherestomi <oawodiya1@gmail.com>
2023-03-08 12:03:44 -0500

    Revert "resolved relational value error"
    This reverts commit fda79ee4f6347fb54c36afd6f941fcd1d55399a2.

--- backend.py
+++ backend.py
@@ -8,16 +8,16 @@ notion_token = "secret_omL8nzIdOZySUeAtSOCHm0bUNh2ydXdohuePKPBXkxm"
 # Device Database Connection
 device_url = "https://www.notion.so/0558f54ce2a34a0ea64e7cfee5ad0d7f?v=d2ba830e965f4f329e74a24c7eaab458&pvs=4"
 device_db_id = "0558f54ce2a34a0ea64e7cfee5ad0d7f"
-devices_df = pd.DataFrame(nd.download(device_db_id, api_key=notion_token))
+devices_df = pd.DataFrame(nd.download(device_db_id, api_key=notion_token, resolve_relation_values=True))
 devices = devices_df[["Device Label", "Device Type"]]
 
 # Support Ticket Database Connection
 tickets_url = "https://www.notion.so/f660a37473f4440cb59fff7356f54d0b?v=99f157fd06874637a59f869ad26802e1&pvs=4"
 tickets_db_id = "f660a37473f4440cb59fff7356f54d0b"
-tickets = pd.DataFrame(nd.download(tickets_db_id, api_key=notion_token))
+tickets = pd.DataFrame(nd.download(tickets_db_id, api_key=notion_token, resolve_relation_values=True))
 
 # Employee Database Connection
 ee_db_id = "03764697bdf74f2b938313815cf62069"
 ee_url = "https://www.notion.so/03764697bdf74f2b938313815cf62069?v=e856d446c1a44cfcb8857b014f591284"
-ee_df = pd.DataFrame(nd.download(ee_db_id, api_key=notion_token))
+ee_df = pd.DataFrame(nd.download(ee_db_id, api_key=notion_token, resolve_relation_values=True))
 ee = ee_df[["Full Name", "EE Code"]][ee_df["Status"]=='Active']
